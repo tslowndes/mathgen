ce15f65359d0b5f0c7c62a059a675465bf8194c0
tslowndes <tslowndes@users.noreply.github.com>
2020-12-10 16:54:52 +0000

    changing urls and site name

--- mathgen/settings.py
+++ mathgen/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'mathgen.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mathgen.wsgi.application'
 
 
 # Database
--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('subcontents/', views.subcontents, name='subcontents'),
+    path('qs/', views.index, name='index'),
+    path('', views.subcontents, name='subcontents'),
 ]
