cba15ebd80702fc5c5ef9f897accc79cc89d182b
T Lowndes <tslowndes@outlook.com>
2021-03-12 13:35:23 +0000

    adding fractions and integers

--- polls/question_generators/number/fractions.py
+++ polls/question_generators/number/fractions.py
@@ -36,91 +36,104 @@ def gen_mixed_number(n,a,b,c,d,e):
 		return {'count': [i for i in range(0, n)], 'questions': qs, 'answers': ans}
 
 
-def adding_fractions(same_denom=0, unit=0, negative = 0):
+def adding_fractions(same_denom=0, unit=0, negative = 0, whole = 0):
 
-	subtract = random.randint(0,1)
+    subtract = random.randint(0,1)
 
-	if same_denom == 0:
-		d1 = random.randint(2,8)
-		d2 = random.randint(d1,10)
-		while d1 == d2:
-			d2 = random.randint(d1, 10)
-	else:
-		d1 = random.randint(3,10)
-		d2 = d1
+    if same_denom == 0:
+	    d1 = random.randint(2,8)
+	    d2 = random.randint(d1,10)
+	    while d1 == d2:
+	        d2 = random.randint(d1, 10)
+    else:
+        d1 = random.randint(3,10)
+        d2 = d1
 
-	if unit == 0:
-		n1 = random.randint(1, d1-1)
-		n2 = random.randint(1, d2-1)
-		if n1 == n2:
-			if n2 - 1 > 0:
-				n2 = n2 - 1
-			elif n1 + 1 < d1:
-				n1 = n1 + 1
-	else:
-		n1, n2 = 1,1
+    if unit == 0:
+        if whole == 1:
+            n1 = d1 * random.randint(1,4)
+        else:
+            n1 = random.randint(1, d1-1)
 
-	if negative == 0:
-		if n2/d2 > n1/d1:
-			n1,n2 = n2,n1
-			d1,d2 = d2,d1
+        n2 = random.randint(1, d2-1)
+
+        if whole == 0:
+            if n1 == n2:
+                if n2 - 1 > 0:
+                    n2 = n2 - 1
+                elif n1 + 1 < d1:
+                    n1 = n1 + 1
+    else:
+        n1, n2 = 1,1
+
+    if negative == 0:
+	    if n2/d2 > n1/d1:
+		    n1,n2 = n2,n1
+		    d1,d2 = d2,d1
 
 
+    if whole == 0:
+        if subtract == 0:
+            question = r'$\frac{' + str(n1) + '}{' + str(d1) + r'} + \frac{' + str(n2) + '}{' + str(d2) + '}$'
+        else:
+            question = r'$\frac{' + str(n1) + '}{' + str(d1) + r'} - \frac{' + str(n2) + '}{' + str(d2) + '}$'
+    else:
+        if subtract == 0:
+            question = r'$' + str(round(n1/d1, 0)).strip('.0') + r' + \frac{' + str(n2) + '}{' + str(d2) + '}$'
+        else:
+            question = r'$' + str(round(n1/d1, 0)).strip('.0') + r' - \frac{' + str(n2) + '}{' + str(d2) + '}$'
 
-	if subtract == 0:
-		question = r'$\frac{' + str(n1) + '}{' + str(d1) + r'} + \frac{' + str(n2) + '}{' + str(d2) + '}$'
-	else:
-		question = r'$\frac{' + str(n1) + '}{' + str(d1) + r'} - \frac{' + str(n2) + '}{' + str(d2) + '}$'
 
-	com_d = lcm(d1,d2)
+    com_d = lcm(d1,d2)
 
-	d3 = com_d
-	if subtract == 0:
-		n3 = int(n1 * (com_d/d1)) + int(n2 * (com_d/d2))
-	else:
-		n3 = int(n1 * (com_d / d1)) - int(n2 * (com_d / d2))
+    d3 = com_d
+    if subtract == 0:
+        n3 = int(n1 * (com_d/d1)) + int(n2 * (com_d/d2))
+    else:
+        n3 = int(n1 * (com_d / d1)) - int(n2 * (com_d / d2))
 
-	n4, d4 = simplify_frac(abs(n3), d3)
+    n4, d4 = simplify_frac(abs(n3), d3)
 
-	n4, d4 = str(n4), str(d4)
-	if str(n4)[0] != '0':
-		n4 = str(n4).strip('0')
-		d4 = str(d4).strip('0')
-	else:
-		n4 = n4[:-2]
-		d4 = d4[:-2]
+    n4, d4 = str(n4), str(d4)
+    if str(n4)[0] != '0':
+	    n4 = str(n4).strip('0')
+	    d4 = str(d4).strip('0')
+    else:
+        n4 = n4[:-2]
+        d4 = d4[:-2]
 
-	print(n4)
+    print(n4)
 
-	if n4[-1] == '.':
-		n4 = n4[:-1]
-	if d4[-1] == '.':
-		d4 = d4[:-1]
-	n3 = int(n3)
-	n4 = int(n4)
-	if n3 == d3:
-		answer = r'$\frac{' + str(n3) + '}{' + str(d3) + r'} = 1 $'
-	else:
-		if n3 == n4:
-			if n3 > 0:
-				answer = r'$\frac{' + str(n3) + '}{' + str(d3) + '}$'
-			elif n3 == 0:
-				answer = r'$0$'
-			else:
-				answer = r'$-\frac{' + str(abs(n3)) + '}{' + str(d3) + '}$'
-		else:
-			if n3 > 0:
-				answer = r'$\frac{' + str(n3) + '}{' + str(d3) + r'} = \frac{' + str(n4) + '}{' + str(d4) + '}$'
-			else:
-				answer = r'$-\frac{' + str(abs(n3)) + '}{' + str(d3) + r'} = -\frac{' + str(abs(n4)) + '}{' + str(d4) + '}$'
+    if n4[-1] == '.':
+        n4 = n4[:-1]
+    if d4[-1] == '.':
+        d4 = d4[:-1]
+    n3 = int(n3)
+    n4 = int(n4)
 
-	return question, answer
+    if n3 == d3:
+        answer = r'$\frac{' + str(n3) + '}{' + str(d3) + r'} = 1 $'
+    else:
+        if n3 == n4:
+            if n3 > 0:
+                answer = r'$\frac{' + str(n3) + '}{' + str(d3) + '}$'
+            elif n3 == 0:
+                answer = r'$0$'
+            else:
+                answer = r'$-\frac{' + str(abs(n3)) + '}{' + str(d3) + '}$'
+        else:
+            if n3 > 0:
+                answer = r'$\frac{' + str(n3) + '}{' + str(d3) + r'} = \frac{' + str(n4) + '}{' + str(d4) + '}$'
+            else:
+                answer = r'$-\frac{' + str(abs(n3)) + '}{' + str(d3) + r'} = -\frac{' + str(abs(n4)) + '}{' + str(d4) + '}$'
 
-def gen_adding_fractions(n=10,same_denom=0,unit=0,d=0,e=0,f=0):
+    return question, answer
+
+def gen_adding_fractions(n=10,same_denom=0,unit=0,whole=0,e=0,f=0):
 	qs = []
 	ans = []
 	for i in range(n):
-		q, a = adding_fractions(same_denom, unit)
+		q, a = adding_fractions(same_denom, unit, 0, whole)
 
 		qs.append(q)
 		ans.append(a)
@@ -171,7 +184,7 @@ def multiplying_fraction_by_integer():
 		n4 = n4[:-1]
 	if d4[-1] == '.':
 		d4 = d4[:-1]
-		
+
 	n3 = int(n3)
 	n4 = int(n4)
 
