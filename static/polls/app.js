c1a72b6cc0c09c97acb9620b2928c43d0b327104
tslowndes <tslowndes@users.noreply.github.com>
2021-03-16 20:46:23 +0000

    touch

--- static/polls/app.js
+++ static/polls/app.js
@@ -1,8 +1,14 @@
 var colour = $(".selected").css("background-color");
-var $canvas = $("canvas");
+//var $canvas = $("canvas");
 var context = $canvas[0].getContext("2d");
 var lastEvent;
-var mouseDown = false;
+//var mouseDown = false;
+
+// Variables for referencing the canvas and 2dcanvas context
+var canvas,ctx;
+
+// Variables to keep track of the mouse position and left-button status
+var mouseX,mouseY,mouseDown=0;
 
 // When clicking on colours items
 $(".controls").on("click", "li", function () {
@@ -32,50 +38,84 @@ function changeColor() {
     $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
 }
 
-// When new colour sliders change
-$("input[type=range]").change(changeColor);
 
 
-// When add colour is pressed
-$("#addNewColor").click(function () {
-    // Append the colours to the controls
-    var $newColor = $("<li></li>");
-    $newColor.css("background-color", $("#newColor").css("background-color"));
-    $(".controls ul").append($newColor);
-    // Select the new added colour
-    $newColor.click();
-});
+// Draws a dot at a specific position on the supplied canvas name
+// Parameters are: A canvas context, the x position, the y position, the size of the dot
+function drawDot(ctx,x,y,size) {
+    // Let's use black by setting RGB values to 0, and 255 alpha (completely opaque)
+    r=0; g=0; b=0; a=255;
 
-// On mouse events on the canvas
-$canvas.mousedown(function (e) {
-    lastEvent = e;
-    mouseDown = true;
-}).mousemove(function (e) {
-    // Draw lines
-    if (mouseDown) {
-        context.beginPath();
-        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-        context.lineTo(e.offsetX, e.offsetY);
-        context.strokeStyle = colour;
-        context.lineWidth = 5;
-        context.lineCap = 'round';
-        context.stroke();
-        lastEvent = e;
+    // Select a fill style
+    ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
+
+    // Draw a filled circle
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+// Clear the canvas context using the canvas width and height
+function clearCanvas(canvas,ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// Keep track of the mouse button being pressed and draw a dot at current location
+function sketchpad_mouseDown() {
+    mouseDown=1;
+    drawDot(ctx,mouseX,mouseY,12);
+}
+
+// Keep track of the mouse button being released
+function sketchpad_mouseUp() {
+    mouseDown=0;
+}
+
+// Keep track of the mouse position and draw a dot if mouse button is currently pressed
+function sketchpad_mouseMove(e) {
+    // Update the mouse co-ordinates when moved
+    getMousePos(e);
+
+    // Draw a dot if the mouse button is currently being pressed
+    if (mouseDown==1) {
+        drawDot(ctx,mouseX,mouseY,12);
     }
-}).mouseup(function () {
-    mouseDown = false;
-}).mouseleave(function () {
-    $canvas.mouseup();
-}).touchstart(function (e){
-    $canvas.mousedown()
-    e.preventDefault()
-}).touchmove(function (e){
-    $canvas.mousemove()
-    e.preventDefault()
-}).touchend(function (e){
-    $canvas.mouseup()
-    e.preventDefault()
-});
+}
+
+// Get the current mouse position relative to the top-left of the canvas
+function getMousePos(e) {
+    if (!e)
+        var e = event;
+
+    if (e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    }
+    else if (e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+ }
+
+
+// Set-up the canvas and add our event handlers after the page has loaded
+function init() {
+    // Get the specific canvas element from the HTML document
+    canvas = document.getElementById('mainCanvas');
+
+    // If the browser supports the canvas tag, get the 2d drawing context for this canvas
+    if (canvas.getContext)
+        ctx = canvas.getContext('2d');
+
+    // Check that we have a valid context to draw on/with before adding event handlers
+    if (ctx) {
+        canvas.addEventListener('mousedown', sketchpad_mouseDown, false);
+        canvas.addEventListener('mousemove', sketchpad_mouseMove, false);
+        window.addEventListener('mouseup', sketchpad_mouseUp, false);
+    }
+}
+
 
 // Clear the canvas when button is clicked
 function clear_canvas_width() {
