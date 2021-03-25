dba165d87c1a6ce1f21c345896a41415ef95888e
T Lowndes <tslowndes@outlook.com>
2021-03-25 12:46:02 +0000

    linear sequences from nth

--- polls/question_generators/algebra/linear_sequences.py
+++ polls/question_generators/algebra/linear_sequences.py
@@ -1,7 +1,7 @@
 import os, sys
 sys.path.append(os.path.join('../../', "lib"))
 from polls.question_generators.tools import *
-from math import abs
+
 
 def gen_find_the_next_term(a1,a2,a3,a4,a5,a6):
     questions = []
@@ -41,9 +41,9 @@ def find_the_next_term(add_or_subtract, neg_first_term, a3=0, a4=0, a5=0, a6=0):
 
 def generate_from_words(min_first_term, negs, lang):
     first_term = random.randint(min_first_term,10)
-    common_difference = random.randint(-10,10)
+    common_difference = rand_no0_no1(-10,10)
 
-    if negs == 0:
+    if negs == 0 and common_difference < 0:
         first_term = first_term + 5 * abs(common_difference)
 
 
@@ -65,10 +65,10 @@ def gen_generate_from_words(n,a,b,c,d,e):
     count = [i for i in range(10)]
 
     for i in range(10):
-        if i < 2:
+        if i < 3:
             q, a = generate_from_words(3,0,0)
-        elif i < 4:
-            q,a = generate_from_words(3,1,1)
+        elif i < 5:
+            q,a = generate_from_words(3,0,1)
         else:
             q,a = generate_from_words(-10,1,1)
 
@@ -76,5 +76,69 @@ def gen_generate_from_words(n,a,b,c,d,e):
         answers.append(a)
 
     return {'questions':questions, 'answers':answers, 'count':count}
+
+def th_suffix(n):
+    if str(n)[-1] == '1':
+        return 'st'
+    elif str(n)[-1] == '2':
+        return 'nd'
+    elif str(n)[-1] == '3':
+        return 'rd'
+    else:
+        return 'th'
+
+def generate_from_nth(negx, first_5_or_random):
+    if negx == 0:
+        a = random.randint(1,10)
+        b = rand_no0_no1(-10,10)
+    else:
+        a = rand_no0_no1(-10,10)
+        b = rand_no0_no1(-10,10)
+
+    if first_5_or_random == 0:
+        q = '$' + tidy_algebra(str(a) + 'n + ' + str(b)) + '$ Find the first five terms.'
+        ans = str(a*1+b) + ', ' + str(a*2+b) + ', ' + str(a*3+b) + ', ' + str(a*4+b) + ', ' + str(a*5+b)
+    else:
+        n = random.randint(10,50)
+        q = '$' + tidy_algebra(str(a) + 'n + ' + str(b)) + '$ Find the ' + str(n) + th_suffix(n) + ' term.'
+        ans = str(a*1+b) + ', ' + str(a*2+b) + ', ' + str(a*3+b) + ', ' + str(a*4+b) + ', ' + str(a*5+b)
+    return q,ans
+
+def gen_generate_from_nth(n,a,b,c,d,e):
+    questions = []
+    answers = []
+    count = [i for i in range(10)]
+
+    for i in range(10):
+        if i < 5:
+            q,a = generate_from_nth(0,0)
+        elif i < 8:
+            q,a = generate_from_nth(0,1)
+        else:
+            q,a = generate_from_nth(1,1)
+
+        questions.append(q)
+        answers.append(a)
+
+    return {'questions':questions, 'answers':answers, 'count':count}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
